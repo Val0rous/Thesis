118672ae65578717d5662814ea1e1d080d423c3f
Francesco Valentini <fv26012001@gmail.com>
2025-01-06 03:19:42 +0100

    Initial movements

--- bologna-wifi-map/src/backend/models/movements.php
+++ bologna-wifi-map/src/backend/models/movements.php
@@ -2,44 +2,17 @@
 header('Content-Type: application/json');
 // First data from April 1, 2021
 $apiUrl = "https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/"
-    . "bolognawifi-matrice-spostamenti/records?order_by=data_evento%20asc&refine=data_evento%3A";
+    . "bolognawifi-matrice-spostamenti/records?select=data_evento%2C%20hour%2C%20giorno%2C%20percentile_50%2C%20tot_pass%2C%20area_from%2C%20area_to&order_by=area_from%2C%20area_to%2C%20hour%20asc&refine=data_evento%3A";
 $totalCountUrl = "https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/"
     . "bolognawifi-matrice-spostamenti/records?order_by=data_evento%20asc&limit=0";
-//$startDate = new DateTime("2021-04-01");
-$startDate = new DateTime("2024-12-01");    // debug
+$startDate = new DateTime("2021-04-01");
+//$startDate = new DateTime("2025-01-03");    // debug
 $endDate = new DateTime();
 $mergedList = [];
 $totalCount = 0;
 $fetchedCount = 0;
 
-function fetch_api_data($url): bool|string
-{
-    $response = file_get_contents($url);
-    if ($response === false) {
-        error_log("Failed to fetch data from URL: $url");
-        return false;
-    }
-    return $response;
-
-//    $ch = curl_init();
-//    curl_setopt($ch, CURLOPT_URL, $url);
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the response as a string
-//    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true); // Verify SSL certificates
-//    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-//
-//    $response = curl_exec($ch);
-//
-//    if (curl_errno($ch)) {
-//        error_log('cURL error: ' . curl_error($ch));
-//        curl_close($ch);
-//        return false;
-//    }
-//
-//    curl_close($ch);
-//    return $response;
-}
-
-$response = fetch_api_data($totalCountUrl);
+$response = file_get_contents($totalCountUrl);
 if ($response !== false) {
     $totalCount = json_decode($response, true)["total_count"];
 } else {
@@ -51,7 +24,7 @@ for ($date = $startDate; $date <= $endDate; $date->modify("+1 day")) {
     $formattedDate = $date->format("Y-m-d");
     echo "\r" . $formattedDate;
     $url = $apiUrl . $formattedDate;
-    $response = fetch_api_data($url);
+    $response = file_get_contents($url);
 
     if ($response !== false) {
         $data = json_decode($response, true);
@@ -65,6 +38,7 @@ for ($date = $startDate; $date <= $endDate; $date->modify("+1 day")) {
         error_log("Failed to fetch data for date $formattedDate");
     }
 }
-
+echo count($mergedList) . PHP_EOL;
 echo PHP_EOL . $fetchedCount . PHP_EOL;
-echo json_encode($mergedList, JSON_PRETTY_PRINT);
+//echo json_encode($mergedList, JSON_PRETTY_PRINT);
+
