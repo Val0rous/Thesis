0864ca75f5c219bd3f272dbcd0f802b19949025c
Francesco Valentini <fv26012001@gmail.com>
2025-01-13 23:03:15 +0100

    Remove useless function

--- bologna-wifi-map/src/backend/utils/utils.php
+++ bologna-wifi-map/src/backend/utils/utils.php
@@ -14,19 +14,3 @@ function extractHourFromTimestamp(string $timestamp): int
     // Extract the hour part between 'T' and the first ':'
     return (int)substr($timestamp, strpos($timestamp, 'T') + 1, 2);
 }
-
-function normalizeArrayTo24(array $input): array
-{
-    $targetLength = 24;
-    $currentLength = count($input);
-
-    if ($currentLength < $targetLength) {
-        // Fill empty positions with zeros
-        $input = array_merge($input, array_fill(0, $targetLength - $currentLength, 0));
-    } elseif ($currentLength > $targetLength) {
-        // Remove extra elements
-        $input = array_slice($input, 0, $targetLength);
-    }
-
-    return $input;
-}
