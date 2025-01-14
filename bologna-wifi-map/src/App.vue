f229669aac6ad0c65e497a26852645669de70c32
Francesco Valentini <fv26012001@gmail.com>
2025-01-14 16:44:20 +0100

    Test polygon on map

--- bologna-wifi-map/src/App.vue
+++ bologna-wifi-map/src/App.vue
@@ -1,44 +1,10 @@
 <script setup>
 
-import {onMounted, ref} from "vue"
-import L from "leaflet"
-
-const lat = ref(0)
-const lng = ref(0)
-const map = ref()
-const mapContainer = ref()
-
-onMounted(() => {
-  map.value = L.map(mapContainer.value).setView([51.505, -0.09], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map.value);
-})
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(position => {
-      lat.value = position.coords.latitude;
-      lng.value = position.coords.longitude;
-      map.value.setView([lat.value, lng.value, 13]);
-
-      L.marker([lat.value, lng.value], {draggable: false})
-          .addTo(map.value)
-          .on("dragend", (e) => {
-            console.log(e);
-          })
-    })
-  }
-}
-
+import Map from "@/frontend/components/Map.vue";
 </script>
 
 <template>
-  <button @click="getLocation">Get Location</button>
-  {{ lat }} , {{ lng }}
-
-  <div ref="mapContainer" style="width: 400px; height: 400px;"></div>
+  <Map/>
 </template>
 
 <style scoped>
--- bologna-wifi-map/src/frontend/components/Map.vue
+++ bologna-wifi-map/src/frontend/components/Map.vue
@@ -1,11 +1,57 @@
 <script setup>
+import {onMounted, ref} from "vue"
+import L from "leaflet"
 
+const lat = ref(0)
+const lng = ref(0)
+const map = ref()
+const mapContainer = ref()
+
+onMounted(() => {
+  map.value = L.map(mapContainer.value).setView([44.4949, 11.3426], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map.value);
+  L.polygon([
+    [44.495, 11.34],
+    [44.497, 11.35],
+    [44.493, 11.36],
+    [44.495, 11.34],
+  ], {
+    color: "red",
+    fillColor: "red",
+    fillOpacity: 0.2,
+  }).addTo(map.value);
+})
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(position => {
+      lat.value = position.coords.latitude;
+      lng.value = position.coords.longitude;
+      map.value.setView([lat.value, lng.value, 13]);
+
+      L.marker([lat.value, lng.value], {draggable: false})
+          .addTo(map.value)
+          .on("dragend", (e) => {
+            console.log(e);
+          })
+    })
+  }
+}
 </script>
 
 <template>
-  <div id="map"></div>
+  <!--    <button @click="getLocation">Get Location</button>-->
+  <!--  {{ lat }} , {{ lng }}-->
+
+  <div ref="mapContainer" class="map"></div>
 </template>
 
 <style scoped>
-
+.map {
+  width: 960px;
+  height: 540px;
+}
 </style>
