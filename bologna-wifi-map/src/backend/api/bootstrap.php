2a4e962b4196b63d83d0048077b7ef6fd87dd250
Francesco Valentini <fv26012001@gmail.com>
2025-01-15 18:46:15 +0100

    Adjust CORS to allow for network connections

--- bologna-wifi-map/src/backend/api/bootstrap.php
+++ bologna-wifi-map/src/backend/api/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
+//header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true"); // If you're using cookies or credentials
