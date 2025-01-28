21857557cfed25b0c73c076ccb1a9249b3cfea3d
Francesco Valentini <fv26012001@gmail.com>
2025-01-28 03:05:18 +0100

    Change map layer and add fallback to avoid API limits

--- bologna-wifi-map/src/frontend/components/Map.vue
+++ bologna-wifi-map/src/frontend/components/Map.vue
@@ -64,15 +64,54 @@ const data = {
   polylines: polylines,
   clickedPolygon: clickedPolygon,
 };
+const mapStyle = {
+  openStreetMap: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  openTopoMap: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+  esriStreets: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+  esriSatellite: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+  // esriTerrain: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}',
+  mapTilerStreets: 'https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=sGIp9VsEALaxJyTFqagx',
+  mapTilerTopo: 'https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}.png?key=sGIp9VsEALaxJyTFqagx',
+  mapTilerDataviz: 'https://api.maptiler.com/maps/dataviz/{z}/{x}/{y}.png?key=sGIp9VsEALaxJyTFqagx',
+  mapTilerBasic: 'https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=sGIp9VsEALaxJyTFqagx',
+  mapTilerSatellite: 'https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=sGIp9VsEALaxJyTFqagx',
+  mapTilerHybrid: 'https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=sGIp9VsEALaxJyTFqagx',
+  // usgsSatellite: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',
+  thunderforestTransport: 'https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=61c2e4cabac64561b6187e7869981efe',
+  thunderforestLandscape: 'https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=61c2e4cabac64561b6187e7869981efe',
+  thunderforestAtlas: 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=61c2e4cabac64561b6187e7869981efe',
+  thunderforestNeighborhood: 'https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=61c2e4cabac64561b6187e7869981efe',
+}
+
+const attributions = {
+  openStreetMap: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  openTopoMap: '&copy; <a href="https://opentopomap.org">OpenTopoMap</a> & contributors',
+  esri: '&copy; <a href="https://www.esri.com/">Esri</a> & contributors',
+  mapTiler: '&copy; <a href="https://www.maptiler.com/">MapTiler</a>',
+  // usgs: '&copy; <a href="https://www.usgs.gov/">USGS</a>'
+  thunderforest: '&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>',
+}
 
 onMounted(async () => {
   map.value = L.map(mapContainer.value).setView([44.4949, 11.3426], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  const tileLayer = L.tileLayer(mapStyle.mapTilerStreets, {
     minZoom: 2,
     maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: attributions.mapTiler,
+    errorTileUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg' // Optional placeholder
   }).addTo(map.value);
 
+  const fallbackLayer = L.tileLayer(mapStyle.openStreetMap, {
+    minZoom: 2,
+    maxZoom: 19,
+    attribution: attributions.openStreetMap
+  });
+
+  tileLayer.on("tileerror", function (e) {
+    console.error("Map tile failed. Switching to OpenStreetMap...", e);
+    map.value.removeLayer(tileLayer);
+    fallbackLayer.addTo(map.value);
+  })
   map.value.zoomControl.setPosition("bottomright");
 
   // Prevent scroll propagation for buttons
